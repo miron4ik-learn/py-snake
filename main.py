5ce5d9c81f17de8501b26ccd1cfd3fd93a12d1fa
miron4ik <miron4ik.life@yandex.ru>
2022-09-07 21:50:26 +0300

    Eat food

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import random
 import pygame
 
 pygame.init()
@@ -13,8 +14,18 @@ black = (0, 0, 0)
 segment_size = 20
 snake_speed = 12
 
-head_x = width // 2
-head_y = height // 2
+head_x = width // 2 // segment_size * segment_size
+head_y = height // 2 // segment_size * segment_size
+
+def get_random_point():
+  x = random.randint(0, width - segment_size) // segment_size * segment_size
+  y = random.randint(0, height - segment_size) // segment_size * segment_size
+  return x, y
+
+food_x, food_y = get_random_point()
+
+snake = []
+snake_length = 1
 
 vx = 0
 vy = 0
@@ -45,6 +56,12 @@ def head_move(key):
     
   return vx, vy
 
+def is_loss():
+  return head_x < 0 or head_x > width - segment_size or head_y < 0 or head_y > height - segment_size
+
+def ate():
+  return head_x == food_x and head_y == food_y
+
 def game_over():
   message = font.render('Вы проиграли', True, red)
   display.blit(message, [ width / 2, height / 2 ])
@@ -52,9 +69,17 @@ def game_over():
   
   time.sleep(2)
   exit()
+  
+def show_snake(snake):
+  for x in snake:
+    pygame.draw.rect(display, black, [ x[0], x[1], segment_size, segment_size ])
+
+def show_score(score):
+  value = font.render('Очки: ' + str(score), True, black)
+  display.blit(value, [ 0, 0 ])
 
 while True:
-  if head_x < 0 or head_x > width - segment_size or head_y < 0 or head_y > height - segment_size:
+  if is_loss():
     game_over()
   
   events = pygame.event.get()
@@ -68,7 +93,19 @@ while True:
   head_y += vy
 
   display.fill(green)
-  pygame.draw.rect(display, black, [ head_x, head_y, segment_size, segment_size ])
+  
+  pygame.draw.rect(display, red, [ food_x, food_y, segment_size, segment_size ])
+  
+  snake.append((head_x, head_y))
+  if len(snake) > snake_length:
+    del snake[0]
+  
+  show_snake(snake)
+  show_score(snake_length - 1)
+  
+  if ate():
+    food_x, food_y = get_random_point()
+    snake_length += 1
   
   pygame.display.flip()
   clock.tick(snake_speed)
